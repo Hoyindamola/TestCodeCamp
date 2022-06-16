096b7c4f768625ab3aced9864e3c31a8b93ae509
Hoyindamola <oyindamolaadedoyin2@gmail.com>
2022-06-16 08:27:38 +0100

    myFirstJavaProject

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first Java Code experience with Testify Limited");
+        System.out.println("I never knew that Software Testing requires coding");
+        System.out.println("I am glad that Testify Academy gave us a thorough foundational lesson");
+    }
+}
