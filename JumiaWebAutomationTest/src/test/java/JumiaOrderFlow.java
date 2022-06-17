1d07693f054f95b04c7fa122b31d12bb6987f261
Hoyindamola <oyindamolaadedoyin2@gmail.com>
2022-06-17 11:35:08 +0100

    Updated JumiaWebTest

--- JumiaWebAutomationTest/src/test/java/JumiaOrderFlow.java
+++ JumiaWebAutomationTest/src/test/java/JumiaOrderFlow.java
@@ -20,7 +20,7 @@ public class JumiaOrderFlow {
         //locate where the chromedriver is
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
 
-        //1.Open Chrome Browser
+        //1. Open Chrome Browser
         driver = new ChromeDriver();
 
         //2. Open my Jumia Web URL (https://www.jumia.com.ng/)
@@ -59,57 +59,67 @@ public class JumiaOrderFlow {
         Thread.sleep(5000);
 
     }
-    public class myTest{
-        @Test (priority = 0)
-        public void emptyFields () throws InterruptedException {
+
+    public class myTest {
+        @Test(priority = 0)
+        public void emptyFields() throws InterruptedException {
             //Do not enter a value on the email field
             //Do not enter a value on the password field
             // Click on the LOGIN Button
             driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
+            System.out.println("Login failed due to empty form fields. Trying again ..");
 
             Thread.sleep(5000);
 
 
         }
-        @Test (priority = 1)
-        public void invalidEmail () throws InterruptedException {
+
+        @Test(priority = 1)
+        public void invalidEmail() throws InterruptedException {
             //Input invalid email address on the email field
-            driver.findElement(By.id("fi-email")).sendKeys("oyindamolaa");
+            driver.findElement(By.id("fi-email")).sendKeys("");
             //Input valid password on the password field
-            driver.findElement(By.id("fi-password")).sendKeys("pretty24");
+            driver.findElement(By.id("fi-password")).sendKeys("");
             // Click on the LOGIN Button
             driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
+            System.out.println("Another Login attempt failed due to an invalid email input. Try again");
 
             Thread.sleep(5000);
         }
-        @Test (priority = 2)
-        public void invalidPassword () throws InterruptedException {
+
+        @Test(priority = 2)
+        public void invalidPassword() throws InterruptedException {
             //clear existing value on the email field
             driver.findElement(By.id("fi-email")).clear();
             //Input valid email address on the email field
-            driver.findElement(By.id("fi-email")).sendKeys("oyindamolaadedoyin2@gmail.com");
+            driver.findElement(By.id("fi-email")).sendKeys("");
             // clear existing password
             driver.findElement(By.id("fi-password")).clear();
             //input invalid password
-            driver.findElement(By.id("fi-password")).sendKeys("prey");
+            driver.findElement(By.id("fi-password")).sendKeys("");
             // Click on the LOGIN Button
             driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
+            System.out.println("Another Login attempt failed due to an invalid password input. Try again");
             Thread.sleep(5000);
 
         }
-        @Test (priority = 3)
-        public void successfulLoginTest () throws InterruptedException {
+
+        @Test(priority = 3)
+        public void successfulLoginTest() throws InterruptedException {
             //clear existing value on the email field
             driver.findElement(By.id("fi-email")).clear();
             //Input valid email address on the email field
-            driver.findElement(By.id("fi-email")).sendKeys("oyindamolaadedoyin2@gmail.com");
+            driver.findElement(By.id("fi-email")).sendKeys("");
             // clear existing password
             driver.findElement(By.id("fi-password")).clear();
             //Input valid password on the password field
-            driver.findElement(By.id("fi-password")).sendKeys("pretty24");
+            driver.findElement(By.id("fi-password")).sendKeys("");
             // Click on the LOGIN Button
             driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
-            //
+            System.out.println("Yipee!!, Successful Login");
+            Thread.sleep(15000);
+        }
+
             //String expectedElement = "";
             //WebElement user = driver.findElement(By.xpath("//*[@id=\"jm\"]/header/section/div[2]/div[1]/label/text()"));
 
@@ -117,25 +127,22 @@ public class JumiaOrderFlow {
             //System.out.println("You're successfully logged in");
             //else
             // System.out.println("Login not successful");
-            Thread.sleep(15000);
+
             // Click on the LOGIN Button again (after passing Google Captcha)
             //driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
 
-        }
-        //public void myAccountPage(){
-            //take me to my account page
-            //https://www.jumia.com.ng/customer/account/index/
-        //}
-        @Test (priority = 4)
-        public void logout() throws InterruptedException {
-            //Locate and click on User Profile name button to locate the Logout button
-            driver.findElement(By.xpath("//*[@id=\"jm\"]/header/section/div[2]/div[1]/label")).click();
-            //Locate and Click on the LOGOUT button to log out of Jumia web App
-            driver.findElement(By.xpath("//*[@id=\"dpdw-login-box\"]/div/form/button")).click();
-            Thread.sleep(5000);
+            @Test(priority = 4)
+            public void logout () throws InterruptedException {
+                //Locate and click on User Profile name button to locate the Logout button
+                driver.findElement(By.xpath("//*[@id=\"jm\"]/header/section/div[2]/div[1]/label")).click();
+                //Locate and Click on the LOGOUT button to log out of Jumia web App
+                driver.findElement(By.xpath("//*[@id=\"dpdw-login-box\"]/div/form/button")).click();
+                System.out.println("Now logging out!");
+                Thread.sleep(5000);
+            }
+
         }
 
-    }
     @AfterTest
     public void quitBrowser() {
         //Quit the browser
