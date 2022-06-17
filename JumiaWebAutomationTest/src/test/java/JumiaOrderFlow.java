58a7fa1f361325cf2fe679d936f8324cbd0809c8
Hoyindamola <oyindamolaadedoyin2@gmail.com>
2022-06-17 10:25:53 +0100

    Updated Tests and Allure Results

--- JumiaWebAutomationTest/src/test/java/JumiaOrderFlow.java
+++ JumiaWebAutomationTest/src/test/java/JumiaOrderFlow.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+
+import static org.testng.TestRunner.PriorityWeight.priority;
 
 public class JumiaOrderFlow {
 
@@ -55,30 +59,87 @@ public class JumiaOrderFlow {
         Thread.sleep(5000);
 
     }
-    @Test
-    public void loginTest() throws InterruptedException {
-        //Input email address on the email field
-        driver.findElement(By.id("fi-email")).sendKeys("");
-        //Input your password on the username field
-        driver.findElement(By.id("fi-password")).sendKeys("");
-        // Click on the LOGIN Button
-        driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
-        Thread.sleep(5000);
+    public class myTest{
+        @Test (priority = 0)
+        public void emptyFields () throws InterruptedException {
+            //Do not enter a value on the email field
+            //Do not enter a value on the password field
+            // Click on the LOGIN Button
+            driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
+
+            Thread.sleep(5000);
 
 
+        }
+        @Test (priority = 1)
+        public void invalidEmail () throws InterruptedException {
+            //Input invalid email address on the email field
+            driver.findElement(By.id("fi-email")).sendKeys("oyindamolaa");
+            //Input valid password on the password field
+            driver.findElement(By.id("fi-password")).sendKeys("pretty24");
+            // Click on the LOGIN Button
+            driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
 
-    //@Test (priority = 1)
-    //public void logout() throws InterruptedException {
-        //Locate and click on User Profile name button to locate the Logout button
-        driver.findElement(By.xpath("//*[@id=\"jm\"]/header/section/div[2]/div[1]/label")).click();
-        //Locate and Click on the LOGOUT button to log out of Jumia web App
-        //driver.findElement(By.xpath("/html/body/div[1]/header/section/div[2]/div[1]/div/div/form/button")).click();
-        Thread.sleep(5000);
+            Thread.sleep(5000);
+        }
+        @Test (priority = 2)
+        public void invalidPassword () throws InterruptedException {
+            //clear existing value on the email field
+            driver.findElement(By.id("fi-email")).clear();
+            //Input valid email address on the email field
+            driver.findElement(By.id("fi-email")).sendKeys("oyindamolaadedoyin2@gmail.com");
+            // clear existing password
+            driver.findElement(By.id("fi-password")).clear();
+            //input invalid password
+            driver.findElement(By.id("fi-password")).sendKeys("prey");
+            // Click on the LOGIN Button
+            driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
+            Thread.sleep(5000);
+
+        }
+        @Test (priority = 3)
+        public void successfulLoginTest () throws InterruptedException {
+            //clear existing value on the email field
+            driver.findElement(By.id("fi-email")).clear();
+            //Input valid email address on the email field
+            driver.findElement(By.id("fi-email")).sendKeys("oyindamolaadedoyin2@gmail.com");
+            // clear existing password
+            driver.findElement(By.id("fi-password")).clear();
+            //Input valid password on the password field
+            driver.findElement(By.id("fi-password")).sendKeys("pretty24");
+            // Click on the LOGIN Button
+            driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
+            //
+            //String expectedElement = "";
+            //WebElement user = driver.findElement(By.xpath("//*[@id=\"jm\"]/header/section/div[2]/div[1]/label/text()"));
+
+            //if (user.equals(expectedElement))
+            //System.out.println("You're successfully logged in");
+            //else
+            // System.out.println("Login not successful");
+            Thread.sleep(15000);
+            // Click on the LOGIN Button again (after passing Google Captcha)
+            //driver.findElement(By.xpath("//*[@id=\"authForm\"]/button/span")).click();
+
+        }
+        //public void myAccountPage(){
+            //take me to my account page
+            //https://www.jumia.com.ng/customer/account/index/
+        //}
+        @Test (priority = 4)
+        public void logout() throws InterruptedException {
+            //Locate and click on User Profile name button to locate the Logout button
+            driver.findElement(By.xpath("//*[@id=\"jm\"]/header/section/div[2]/div[1]/label")).click();
+            //Locate and Click on the LOGOUT button to log out of Jumia web App
+            driver.findElement(By.xpath("//*[@id=\"dpdw-login-box\"]/div/form/button")).click();
+            Thread.sleep(5000);
+        }
+
     }
-
     @AfterTest
-    public void quitBrowser(){
+    public void quitBrowser() {
         //Quit the browser
         driver.quit();
     }
 }
+
